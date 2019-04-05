5c3c81a1af58bd6e37d73162ebb48f4997eba565
Jeson1g <jesonb2@126.com>
2019-04-05 21:37:43 +0800

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,8 +14,29 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_start_and_retrieve(self):
         self.browser.get("http://localhost:8000")
-
         self.assertIn("To-Do", self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # 她在一个文本框中输入了“Buy peacock feathers” （购买孔雀羽毛）
+        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+        # 她按回车键后，页面更新了
+        # 待办事项表格中显示了“1: Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail("Finish the test!")
 
 
