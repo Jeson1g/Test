dfa07d73d4c221143705a7978757f3eb21a4dc70
Jeson1g <jesonb2@126.com>
2019-04-07 13:09:46 +0800

    Test post request to save database

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_start_and_retrieve(self):
         self.browser.get("http://localhost:8000")
 
@@ -25,17 +31,14 @@ class NewVisitorTest(unittest.TestCase):
         )
         # 她在一个文本框中输入了“Buy peacock feathers” （购买孔雀羽毛）
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail("Finish the test!")
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from .views import home_page
-from django.http import HttpRequest
+from .models import Item
 
 
 # Create your tests here.
@@ -14,3 +14,28 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_test': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test__saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        items = Item.objects.all()
+
+        self.assertEqual(items.count(), 2)
+
+        first_item = items[0]
+        second_item = items[1]
+        self.assertEqual(first_item.text, 'The first (ever) list item')
+        self.assertEqual(second_item.text, 'Item the second')
