5b3393237ba53ba2a6ad53d7aa8fd14ac6687b0a
Jeson1g <jesonb2@126.com>
2019-04-08 11:03:21 +0800

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
 import time
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -12,14 +13,8 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
     def test_start_and_retrieve(self):
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -31,17 +26,17 @@ class NewVisitorTest(unittest.TestCase):
         )
         # 她在一个文本框中输入了“Buy peacock feathers” （购买孔雀羽毛）
         # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        to_do_thing = 'Use peacock feathers to make a fly'
+        inputbox.send_keys(to_do_thing)
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        table = self.browser.find_element_by_id('id_list_table')
+
+        rows = table.find_elements_by_tag_name('tr')
+        string = ''
+        self.assertIn(to_do_thing, string.join([row.text for row in rows]))
 
         self.fail("Finish the test!")
-
-
-if __name__ == '__main__':
-    unittest.main(warnings="ignore")
