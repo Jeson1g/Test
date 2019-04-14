58195262c337ad39a5de4b01e33d404bdd6d3000
Jeson1g <jesonb2@126.com>
2019-04-14 16:24:03 +0800

    update models

--- lists/models.py
+++ lists/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 
 # Create your models here.
+class List(models.Model):
+
+    class Meta:
+        db_table = 'tb_list'
+        verbose_name = '待办事项清单'
+
+
 class Item(models.Model):
 
     text = models.TextField()
+    list = models.ForeignKey(List, default=None, blank=True)
 
+    class Meta:
+        db_table = 'tb_item'
+        verbose_name = '待办事项'
 
