e0b7e87cf0e1d2376df157783a7b9349266ae201
Jeson1g <jesonb2@126.com>
2019-04-17 07:31:34 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,41 +32,63 @@ class NewVisitorTest(LiveServerTestCase):
                         raise e
                     time.sleep(0.5)
 
-    def test_start_and_retrieve(self):
+    # def test_start_and_retrieve(self):
+    #     self.browser.get(self.live_server_url)
+    #
+    #     # 应用邀请她输入一个待办事项
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Use peacock feathers to make a fly')
+    #     # 她按回车键后，页面更新了
+    #     # 待办事项表格中显示了“1: Buy peacock feathers”
+    #     inputbox.send_keys(Keys.ENTER)
+    #
+    #     self.wait_for_row_in_list_table('1: Use peacock feathers to make a fly')
+    #
+    # def test_multiple_users_can_start_lists_at_different_urls(self):
+    #
+    #     # 伊迪丝新建一个待办事项清单
+    #     self.browser.get(self.live_server_url)
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Buy peacock feathers')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #     # 她注意到清单有个唯一的URL
+    #     edith_list_url = self.browser.current_url
+    #     self.assertRegex(edith_list_url, '/lists/.+')
+    #
+    #     # Use new browser
+    #     self.browser.quit()
+    #     self.browser = webdriver.Firefox()
+    #     self.browser.get(self.live_server_url)
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Buy milk')
+    #     inputbox.send_keys(Keys.ENTER)
+    #
+    #     self.wait_for_row_in_list_table('1: Buy milk')
+
+    def test_layout_and_styling(self):
+
         self.browser.get(self.live_server_url)
 
-        # 应用邀请她输入一个待办事项
+        self.browser.set_window_size(1024, 768)
+
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        # 她按回车键后，页面更新了
-        # 待办事项表格中显示了“1: Buy peacock feathers”
+
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
 
-        self.wait_for_row_in_list_table('1: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: testing')
 
-    def test_multiple_users_can_start_lists_at_different_urls(self):
-
-        # 伊迪丝新建一个待办事项清单
-        self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        # 她注意到清单有个唯一的URL
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
 
-        # Use new browser
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
-
-        self.wait_for_row_in_list_table('1: Buy milk')
-
-
-
-
-
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
