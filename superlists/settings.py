390a6550d7669df95a606e2deffe03f7cf431978
Jeson1g <jesonb2@126.com>
2019-04-17 09:57:19 +0800

    set STATIC_ROOT in settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -117,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
